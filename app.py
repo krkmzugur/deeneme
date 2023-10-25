a75d9b4e03e646fd0636bff5a42bcb39c5253e34
U.K <krkmzugur@gmail.com>
2023-10-25 23:59:06 +0300

    F.C

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+@app.route('/deneme')
+def deneme():  # put application's code here
+    return 'deneme'
+
 
 if __name__ == '__main__':
     app.run()
